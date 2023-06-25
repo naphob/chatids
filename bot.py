c41dbc1d55732508a11d21e7704ca703e9b5ce43
Naphob <naphob@gmail.com>
2023-06-25 14:32:33 +0700

    code refactor

--- bot.py
+++ bot.py
@@ -87,12 +87,7 @@ async def leave(ctx):
 
     await vc.disconnect()
 
-# Command to play voice message in the user's current voice channel
-@client.command(name="say", help="This command will make the bot speak what you want in the voice channel")
-async def say(ctx, *args):
-    user = ctx.message.author
-    username = user.display_name.split('[')
-    text = f'{username[0]} พูดว่า {args}'
+async def tts_vc(ctx, user, text, err_msg):
     if user.voice is not None:
         # put new notification to queue
         q.put(text)
@@ -102,9 +97,9 @@ async def say(ctx, *args):
             vc = ctx.voice_client
         while not q.empty():
             if not vc.is_playing():
-                text = q.get()
-                print(text)
-                sound = gTTS(text=text, lang="th", slow=False)
+                tts_text = q.get()
+                print(tts_text)
+                sound = gTTS(text=tts_text, lang="th", slow=False)
                 sound.save("tts.mp3")
                 source = await discord.FFmpegOpusAudio.from_probe("tts.mp3", method="fallback")
                 vc.play(source)
@@ -116,7 +111,16 @@ async def say(ctx, *args):
             await asyncio.sleep(10)
             await vc.disconnect()
     else:
-        await ctx.send("You are not in a voice channel.")
+        await ctx.send(err_msg)
+
+# Command to play voice message in the user's current voice channel
+@client.command(name="say", help="This command will make the bot speak what you want in the voice channel")
+async def say(ctx, *args):
+    user = ctx.message.author
+    username = user.display_name.split('[')
+    text = f'{username[0]} พูดว่า {args}'
+    err_msg = 'You are not in a voice channel.'
+    await tts_vc(ctx, user, text, err_msg)
 
 # Command to send voice message to mentioned user in a voice channel. The sender doesn't need to connect to that voice channel
 @client.command(name="send", help="This command will send voice message to mentioned user connected to voice channel")
@@ -124,28 +128,7 @@ async def send(ctx, member: discord.Member, *args):
     user = ctx.message.author
     username = user.display_name.split('[')
     text = f'{username[0]} ฝากบอกว่า {args}'
-    if member.voice is not None:
-        q.put(text)
-        try:
-            vc = await member.voice.channel.connect()
-        except:
-            vc = ctx.voice_client
-        while not q.empty():
-            if not vc.is_playing():
-                text = q.get()
-                print(text)
-                sound = gTTS(text=text, lang="th", slow=False)
-                sound.save("tts.mp3")
-                source = await discord.FFmpegOpusAudio.from_probe("tts.mp3", method="fallback")
-                vc.play(source)
-                while vc.is_playing():
-                    await asyncio.sleep(10)
-            else:
-                await asyncio.sleep(10)
-        if q.empty():
-            await asyncio.sleep(10)
-            await vc.disconnect()
-    else:
-        await ctx.send("Receiver is not in a voice channel.")
+    err_msg = 'Receiver is not in a voice channel.'
+    await tts_vc(ctx, user, text, err_msg)
 
 client.run(TOKEN)
