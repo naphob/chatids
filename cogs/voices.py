cc16ab457671505f0a191086fcd920e6bc618592
Naphob <naphob@gmail.com>
2023-07-30 09:07:34 +0700

    add Cogs

---
+++ cogs/voices.py
@@ -0,0 +1,188 @@
+import random
+import discord
+from discord.ext import commands, bridge
+import asyncio
+from gtts import gTTS
+from queue import Queue
+from rich.console import Console
+
+console = Console()
+q = Queue()
+connections = {}
+
+class Voices(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    async def noti(self, member, channel, message):
+        username = member.display_name.split('[')
+        tts_message = f'{username[0]} {message}'
+        if not self.bot.voice_clients:
+            try:
+                vc = await channel.channel.connect()
+            except:
+                vc = self.bot.voice_clients[0]
+                print("Bot is busy in another room")
+            sound = gTTS(text=tts_message, lang="th", slow=False)
+            sound.save("join.mp3")
+            tts_audio_file = await discord.FFmpegOpusAudio.from_probe('join.mp3', method="fallback")
+            vc.play(tts_audio_file)
+            while vc.is_playing():  # Wait for the TTS audio to finish playing
+                await asyncio.sleep(1)
+            console.log(tts_message)
+            await vc.disconnect()
+        else:
+            console.log(f"Bot is still busy at channel {self.bot.voice_clients[0].channel}")
+            await asyncio.sleep(3)
+            if not self.bot.voice_clients:
+                try:
+                    vc = await channel.channel.connect()
+                except:
+                    vc = self.bot.voice_clients[0]
+                    print("Bot is busy in another room")
+                sound = gTTS(text=tts_message, lang="th", slow=False)
+                sound.save("join.mp3")
+                tts_audio_file = await discord.FFmpegOpusAudio.from_probe('join.mp3', method="fallback")
+                vc.play(tts_audio_file)
+                while vc.is_playing():  # Wait for the TTS audio to finish playing
+                    await asyncio.sleep(1)
+                console.log(tts_message)
+                await vc.disconnect()
+
+    async def tts_vc(self, ctx, user, message, err_msg):
+        if user.voice is not None:
+            # put new tts message to queue
+            q.put(message)
+            try:
+                vc = await user.voice.channel.connect()
+            except:
+                vc = ctx.voice_client
+            while not q.empty():
+                if not vc.is_playing():
+                    tts_message = q.get()
+                    console.log(tts_message)
+                    sound = gTTS(text=tts_message, lang="th", slow=False)
+                    sound.save("tts.mp3")
+                    source = await discord.FFmpegOpusAudio.from_probe("tts.mp3", method="fallback")
+                    vc.play(source)
+                    while vc.is_playing():
+                        await asyncio.sleep(3)
+                else:
+                    await asyncio.sleep(8)
+            if q.empty():
+                await asyncio.sleep(3)
+                await vc.disconnect()
+        else:
+            await ctx.respond(err_msg)
+
+    @commands.Cog.listener()
+    async def on_voice_state_update(self, member, before, after):
+        if before.channel is None and after.channel is not None and not after.afk and not member.bot:
+            # A user joined a voice channel
+            message = 'เข้ามาในห้องแล้ว'
+            await self.noti(member, after, message)
+            coins = self.bot.get_cog('Coins')
+            coin = random.random()
+            await coins.add_coin(member, coin, "join vc")
+        elif after.channel and not before.suppress and not before.deaf and not before.mute and not before.self_mute and not before.self_stream and not before.self_video and not before.self_deaf and not after.self_mute and not after.self_stream and not after.self_video and not after.self_deaf and not after.deaf and not after.mute and not after.suppress and not member.bot:
+            # A user moved to another voice channel
+            message = 'ย้ายมาในห้องนี้แล้ว'
+            await self.noti(member, after, message)
+        elif after.channel and before.afk and not after.afk and not member.bot:
+            # A user's back from AFK to voice channel
+            message = 'กลับมาจาก AFK แล้ว'
+            await self.noti(member, after, message)
+        elif after.channel is None and before.channel is not None and not member.bot:
+            # A user left the voice channel
+            message = 'ออกห้องไปแล้ว'
+            await self.noti(member, before, message)
+
+    @bridge.bridge_command(name='summon', help='This command will make the bot join the voice channel')
+    async def summon(self, ctx):
+        """
+        command to join voice channel
+        """
+        if not ctx.author.voice:
+            await ctx.respond(f"{ctx.author.display_name} is not connected to a voice channel")
+            return
+        else:
+            channel = ctx.author.voice.channel
+        await channel.connect()
+
+    @bridge.bridge_command(name='leave', help='This command will make the bot leave the voice channel')
+    async def leave(self, ctx):
+        """
+        command to leave voice channel
+        """
+        vc = ctx.voice_client
+        if not vc:
+            await ctx.respond("I am not connected to a voice channel.")
+            return
+
+        await vc.disconnect()
+
+    @bridge.bridge_command(name="say", help="This command will make the bot speak what you want in the voice channel")
+    async def say(self, ctx, *args):
+        """
+        Command to play voice message in the user's current voice channel
+        """
+        user = ctx.message.author
+        username = user.display_name.split('[')
+        message = f'{username[0]} พูดว่า {args}'
+        err_msg = 'You are not in a voice channel.'
+        await self.tts_vc(ctx, user, message, err_msg)
+
+
+    @bridge.bridge_command(name="send", help="This command will send voice message to mentioned user connected to voice channel")
+    async def send(self, ctx, member: discord.Member, *args):
+        user = ctx.message.author
+        username = user.display_name.split('[')
+        message = f'{username[0]} ฝากบอกว่า {args}'
+        err_msg = 'Receiver is not in a voice channel.'
+        await self.tts_vc(ctx, member, message, err_msg)
+
+    @bridge.bridge_command(name="rec", help="This command will record your voice message then send it to targeted user after stop recording")
+    async def rec(self, ctx, user: discord.Member):  # If you're using commands.Bot, this will also work.
+        voice = ctx.author.voice
+
+        if not voice:
+            await ctx.respond("You aren't in a voice channel!")
+
+        vc = await voice.channel.connect()  # Connect to the voice channel the author is in.
+        connections.update({ctx.guild.id: vc})  # Updating the cache with the guild and channel.
+
+        vc.start_recording(
+            discord.sinks.MP3Sink(),  # The sink type to use.
+            self.once_done,  # What to do once done.
+            user,  # The channel to disconnect from.
+            ctx.author
+        )
+
+    async def once_done(self, sink: discord.sinks, member: discord.Member, user: discord.User, *args):
+        recorded_users = [  # A list of recorded users
+            f"<@{user_id}>"
+            for user_id, audio in sink.audio_data.items()
+        ]
+        await sink.vc.disconnect()  # Disconnect from the voice channel.
+        files = [discord.File(audio.file, f"{user_id}.{sink.encoding}") for user_id, audio in sink.audio_data.items()]  # List down the files.
+        for user_id, audio in sink.audio_data.items():
+            if user_id == user.id:
+                with open("output.mp3", "wb") as f:
+                    f.write(audio.file.getbuffer())
+        vc = await member.voice.channel.connect()
+        playback_voice = await discord.FFmpegOpusAudio.from_probe('output.mp3', method="fallback")
+
+        vc.play(playback_voice)
+        while vc.is_playing():
+            await asyncio.sleep(10)
+        await vc.disconnect()
+
+    @bridge.bridge_command(name="stop_rec", help="This command will stop recording your voice message")
+    async def stop_rec(self, ctx):
+        if ctx.guild.id in connections:  # Check if the guild is in the cache.
+            vc = connections[ctx.guild.id]
+            vc.stop_recording()  # Stop recording, and call the callback (once_done).
+            del connections[ctx.guild.id]  # Remove the guild from the cache.
+
+def setup(bot): # this is called by Pycord to setup the cog
+    bot.add_cog(Voices(bot)) # add the cog to the bot
---
+++ cogs/welcomes.py
@@ -0,0 +1,225 @@
+import os
+import re
+import discord
+import discord.ui
+from discord.ext import commands, bridge
+from dotenv import load_dotenv
+from rich.console import Console
+from PIL import Image, ImageFont, ImageDraw
+
+console = Console()
+load_dotenv()
+
+WELCOME_CHANNEL_ID = 1037740797518430308
+
+class Roles(discord.ui.View):
+    def __init__(self):
+        super().__init__(timeout=None)
+    @discord.ui.button(label="Star Citizen", custom_id="role 1", style=discord.ButtonStyle.primary, emoji="🚀")
+    async def button_callback(self, button, interaction):
+        role = 1124564123640942673
+        user = interaction.user
+        regex = "^(\W|\w)+\[(\w|\W)+\]$"
+        match = re.match(regex, user.display_name)
+        button.disabled = True
+        if role in [r.id for r in user.roles]:
+            button.disabled = True
+        else:
+            if match:
+                await user.add_roles(user.guild.get_role(role))
+                await interaction.response.send_message("คุณได้รับยศแล้ว ขอให้สนุกกับการเล่น Star Citizen", ephemeral = True)
+                console.log(f"Add role {user.guild.get_role(role).name} to {user.display_name}")
+                new_face = 1045127837989994568
+                await user.remove_roles(user.guild.get_role(new_face))
+                console.log(f"Remove role {user.guild.get_role(role).name} from {user.display_name}")
+            else:
+                await interaction.response.send_message("กรุณาเปลี่ยนชื่อให้ถูกต้องตามกฎ เช่น Poon [CaptainWolffe]", ephemeral = True)
+
+    @discord.ui.button(label="เกมอื่นๆ", custom_id="role 2", style=discord.ButtonStyle.success, emoji="🎮")
+    async def button_callback_guest(self, button, interaction):
+        role = 1092322716415172658
+        user = interaction.user
+        regex = "^(\W|\w)+\[(\w|\W)+\]$"
+        match = re.match(regex, user.display_name)
+        button.disabled = True
+        if role in [r.id for r in user.roles]:
+            button.disabled = True
+        else:
+            if match:
+                await user.add_roles(user.guild.get_role(role))
+                await interaction.response.send_message("คุณได้รับยศแล้ว ขอให้สนุกกับการเล่นเกมกับพวกเรา", ephemeral = True)
+                console.log(f"Add role {user.guild.get_role(role).name} to {user.display_name}")
+                new_face = 1045127837989994568
+                await user.remove_roles(user.guild.get_role(new_face))
+                console.log(f"Remove {user.guild.get_role(role).name} role from {user.display_name}")
+            else:
+                await interaction.response.send_message("กรุณาเปลี่ยนชื่อให้ถูกต้องตามกฎ เช่น Poon [CaptainWolffe]", ephemeral = True)
+
+class GetRoles(discord.ui.View):
+    def __init__(self):
+        super().__init__(timeout=None)
+    @discord.ui.button(label="Miner", custom_id="miner", style=discord.ButtonStyle.primary, emoji="⛏️")
+    async def miner_button_callback(self, button, interaction):
+        role = 1004496745700540427
+        user = interaction.user
+        if role in [r.id for r in user.roles]:
+            await user.remove_roles(user.guild.get_role(role))
+            await interaction.response.send_message(f"คุณได้คืนยศ {user.guild.get_role(role).name} แล้ว", ephemeral = True)
+            console.log(f"Remove role {user.guild.get_role(role).name} from {user.display_name}")
+        else:
+            await user.add_roles(user.guild.get_role(role))
+            await interaction.response.send_message(f"คุณได้รับยศ {user.guild.get_role(role).name} แล้ว ขอให้สนุกกับการเล่น Star Citizen", ephemeral = True)
+            console.log(f"Add role {user.guild.get_role(role).name} to {user.display_name}")
+
+    @discord.ui.button(label="Special Forces", custom_id="sf", style=discord.ButtonStyle.primary, emoji="🥷")
+    async def sf_button_callback(self, button, interaction):
+        role = 1004495350977011782
+        user = interaction.user
+        if role in [r.id for r in user.roles]:
+            await user.remove_roles(user.guild.get_role(role))
+            await interaction.response.send_message(f"คุณได้คืนยศ {user.guild.get_role(role).name} แล้ว", ephemeral = True)
+            console.log(f"Remove role {user.guild.get_role(role).name} from {user.display_name}")
+        else:
+            await user.add_roles(user.guild.get_role(role))
+            await interaction.response.send_message(f"คุณได้รับยศ {user.guild.get_role(role).name} แล้ว ขอให้สนุกกับการเล่น Star Citizen", ephemeral = True)
+            console.log(f"Add role {user.guild.get_role(role).name} to {user.display_name}")
+
+    @discord.ui.button(label="Crew", custom_id="crew", style=discord.ButtonStyle.primary, emoji="🧑‍🚀")
+    async def crew_button_callback(self, button, interaction):
+        role = 1013177318686081166
+        user = interaction.user
+        if role in [r.id for r in user.roles]:
+            await user.remove_roles(user.guild.get_role(role))
+            await interaction.response.send_message(f"คุณได้คืนยศ {user.guild.get_role(role).name} แล้ว", ephemeral = True)
+            console.log(f"Remove role {user.guild.get_role(role).name} from {user.display_name}")
+        else:
+            await user.add_roles(user.guild.get_role(role))
+            await interaction.response.send_message(f"คุณได้รับยศ {user.guild.get_role(role).name} แล้ว ขอให้สนุกกับการเล่น Star Citizen", ephemeral = True)
+            console.log(f"Add role {user.guild.get_role(role).name} to {user.display_name}")
+
+    @discord.ui.button(label="Trader", custom_id="trader", style=discord.ButtonStyle.primary, emoji="💰")
+    async def trader_button_callback(self, button, interaction):
+        role = 1008637643866783814
+        user = interaction.user
+        if role in [r.id for r in user.roles]:
+            await user.remove_roles(user.guild.get_role(role))
+            await interaction.response.send_message(f"คุณได้คืนยศ {user.guild.get_role(role).name} แล้ว", ephemeral = True)
+            console.log(f"Remove role {user.guild.get_role(role).name} from {user.display_name}")
+        else:
+            await user.add_roles(user.guild.get_role(role))
+            await interaction.response.send_message(f"คุณได้รับยศ {user.guild.get_role(role).name} แล้ว ขอให้สนุกกับการเล่น Star Citizen", ephemeral = True)
+            console.log(f"Add role {user.guild.get_role(role).name} to {user.display_name}")
+
+    @discord.ui.button(label="Medic", custom_id="medic", style=discord.ButtonStyle.primary, emoji="💉")
+    async def medic_button_callback(self, button, interaction):
+        role = 1134438399957270569
+        user = interaction.user
+        if role in [r.id for r in user.roles]:
+            await user.remove_roles(user.guild.get_role(role))
+            await interaction.response.send_message(f"คุณได้คืนยศ {user.guild.get_role(role).name} แล้ว", ephemeral = True)
+            console.log(f"Remove role {user.guild.get_role(role).name} from {user.display_name}")
+        else:
+            await user.add_roles(user.guild.get_role(role))
+            await interaction.response.send_message(f"คุณได้รับยศ {user.guild.get_role(role).name} แล้ว ขอให้สนุกกับการเล่น Star Citizen", ephemeral = True)
+            console.log(f"Add role {user.guild.get_role(role).name} to {user.display_name}")
+
+    @discord.ui.button(label="Engineer", custom_id="engineer", style=discord.ButtonStyle.primary, emoji="🔧")
+    async def engineer_button_callback(self, button, interaction):
+        role = 1134437490040766515
+        user = interaction.user
+        if role in [r.id for r in user.roles]:
+            await user.remove_roles(user.guild.get_role(role))
+            await interaction.response.send_message(f"คุณได้คืนยศ {user.guild.get_role(role).name} แล้ว", ephemeral = True)
+            console.log(f"Remove role {user.guild.get_role(role).name} from {user.display_name}")
+        else:
+            await user.add_roles(user.guild.get_role(role))
+            await interaction.response.send_message(f"คุณได้รับยศ {user.guild.get_role(role).name} แล้ว ขอให้สนุกกับการเล่น Star Citizen", ephemeral = True)
+            console.log(f"Add role {user.guild.get_role(role).name} to {user.display_name}")
+
+    @discord.ui.button(label="Pilot", custom_id="pilot", style=discord.ButtonStyle.primary, emoji="🛩️")
+    async def pilot_button_callback(self, button, interaction):
+        role = 1134438658829729833
+        user = interaction.user
+        if role in [r.id for r in user.roles]:
+            await user.remove_roles(user.guild.get_role(role))
+            await interaction.response.send_message(f"คุณได้คืนยศ {user.guild.get_role(role).name} แล้ว", ephemeral = True)
+            console.log(f"Remove role {user.guild.get_role(role).name} from {user.display_name}")
+        else:
+            await user.add_roles(user.guild.get_role(role))
+            await interaction.response.send_message(f"คุณได้รับยศ {user.guild.get_role(role).name} แล้ว ขอให้สนุกกับการเล่น Star Citizen", ephemeral = True)
+            console.log(f"Add role {user.guild.get_role(role).name} to {user.display_name}")
+
+class Welcomes(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        console.log(f'{self.bot.user.name} has connected to Discord!')
+        self.bot.add_view(Roles())
+        self.bot.add_view(GetRoles())
+        await self.bot.change_presence(activity=discord.Game(name="Star Citizen"))
+        for guild in self.bot.guilds:
+            # PRINT THE SERVER'S ID AND NAME.
+            console.log(f"- {guild.id} | {guild.name}")
+
+    async def welcome_pic(self, user):
+        channel = await self.bot.fetch_channel(WELCOME_CHANNEL_ID)
+        await user.display_avatar.save('Asset/avatar.png')
+        avatar = Image.open('Asset/avatar.png')
+        count = user.guild.member_count
+        username = user.name
+        text = f"Welcome {username} to IDS"
+        member_text = f"Member #{count}"
+        img =Image.open("Asset/ids_bg.png")
+        W, H = img.size
+        avatar = Image.open("Asset/avatar.png")
+        size = (240, 240)
+        avatar = avatar.resize(size, Image.Resampling.LANCZOS)
+        mask_img = Image.new("L", avatar.size, 0)
+        mask_draw = ImageDraw.Draw(mask_img)
+        mask_draw.ellipse((0, 0) + avatar.size, fill=255)
+        mask_img.save("Asset/mask_circle.jpg", quality=95)
+        draw = ImageDraw.Draw(img)
+        font = ImageFont.truetype("arial.ttf", 50)
+        count_font = ImageFont.truetype("arial.ttf", 32)
+
+        img.paste(avatar ,(440, 80), mask_img)
+        text_size =draw.textlength(text, font=font)
+        count_size =draw.textlength(member_text, font=count_font)
+        draw.text(((W-text_size)/ 2, 340), text, fill=(255, 255, 255, 255), font=font, aligh="center")
+        draw.text(((W-count_size)/ 2, 400), member_text, fill="grey", font=count_font, aligh="center")
+        img.save('Asset/text.png')
+
+        embed = discord.Embed(
+            title = "Welcome to the verse",
+            description=f"ยินดีต้อนรับคุณ <@{user.id}>  สู่ Invicta Defense Service!",
+            color=discord.Color.dark_purple()
+        )
+        rule = "1. เปลี่ยนชื่อ : `ชื่อเล่น [ชื่อในเกม]`\nตัวอย่างการเปลี่ยนชื่อ : `Poon [CaptainWolffe]`\n2. กดรับยศเพื่อเห็นห้องของเกมที่จะเล่น"
+        embed.add_field(name="อย่าลืม!", value=rule)
+
+        await channel.send(embed=embed, file= discord.File('Asset/text.png'), view=Roles())
+
+    @commands.Cog.listener()
+    async def on_member_join(self, member):
+        role = 1045127837989994568 #give new_face role to new joiner so they can see welcome chanel
+        await member.add_roles(member.guild.get_role(role))
+        await self.welcome_pic(member)
+
+    @bridge.bridge_command(name="welcome", help="Welcome new member")
+    async def welcome(self,ctx, user: discord.Member):
+        await self.welcome_pic(user)
+
+    @bridge.bridge_command(name="role", help="Select a role")
+    async def role(self, ctx):
+        embed = discord.Embed(
+                title = "Welcome to the verse",
+                description=f"เลือก Role ที่เหมาะสมกับคุณ เพื่อใช้ในการสื่อสารภายในดิสคอร์ด",
+                color=discord.Color.dark_purple()
+            )
+
+        await ctx.respond(embed=embed, view=GetRoles())
+
+def setup(bot): # this is called by Pycord to setup the cog
+    bot.add_cog(Welcomes(bot)) # add the cog to the bot
