bc8c1428f82f3ca79559eecf2108c743ece3d6ba
naphob <naphob@gmail.com>
2024-11-28 18:13:42 +0700

    fix voice notification

--- cogs/voices.py
+++ cogs/voices.py
@@ -111,7 +111,7 @@ class Voices(commands.Cog):
                 temp_channel = await after.channel.clone(name=f"🚀Gaming 0{room_no}")
                 if temp_channel is not None:
                     await member.move_to(temp_channel)
-            elif after_match is None:
+            elif before.channel is not None and after_match is None:
                 console.log(match)
                 message = 'ย้ายมาในห้องนี้แล้ว'
                 await self.noti(member, after, message)
